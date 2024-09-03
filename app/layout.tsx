a196164cbf9095e1ad951a5350a4ad1643ddf21e
ThePjuns <stefandhh@hotmail.com>
2024-09-03 11:56:52 +0200

    Search works now

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
  
+export const experimental_ppr = true; //added in chapter 10
 export default function RootLayout({
   children,
 }: {
--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+//const nextConfig = {}; changed to the lines below at chapter 10. Does give me a minor error tho. in here and in dashboard/layout
+
+const nextConfig = {
+    experimental: {
+      ppr: 'incremental',
+    },
+  };
 
 export default nextConfig;
